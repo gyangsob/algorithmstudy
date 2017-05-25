ed39d634deb54281a5f4ae8b1ed60912e925eb6c
Gyangob <shmp0125@gmail.com>
2017-05-25 19:47:37 +0900

    [Algorithm][Study][1st week] add comment

--- endian/endian.c
+++ endian/endian.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+// defint macro 
+// bit shift operation to change big endian to little endian
+
+// [X000] --> [000X] : 0xff000000 마스킹 후 3바이트	right shift 
+// [0X00] --> [00X0] : 0x00ff0000 마스킹 후 1바이트 right shift
+// [00X0] --> [0X00] : 0x0000ff00 마스킹 후 1바이트 left shift
+// [000X] --> [X000] : 0x000000ff 마스킹 후 3바이트 left shift
 #define SWAP32(l) \
 	( ((((l) & 0xff000000) >> 24) | \
 	   (((l) & 0x00ff0000) >> 8) | \
