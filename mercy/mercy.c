eee3efe17d64abca640e36c8828bb6c778593a70
Gyangob <shmp0125@gmail.com>
2017-05-25 19:47:37 +0900

    [Algorithm][Study][1st week] add comment

--- mercy/mercy.c
+++ mercy/mercy.c
@@ -5,7 +5,8 @@ int main(void)
 	unsigned int input_num; // Input contains just one positive integer N
 	scanf("%d", &input_num);
 
-	while(input_num > 0) {
+	// for optimization...
+	while(input_num > 0) { 
 		printf("Hello Algospot!\n");
 		input_num--;
 	}
