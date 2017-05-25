4ef338d0175b96e11ef906b9341b0fcb6bb9c3bf
Gyangob <shmp0125@gmail.com>
2017-05-25 19:39:54 +0900

    [Algorithm][Study][1st week] add comment and free

--- draw/draw.c
+++ draw/draw.c
@@ -14,6 +14,8 @@ int main(void)
 	int i, t;
 
 	scanf("%d", &t);
+
+	// 입력받은 TC개수만큼 int 변수 dynamic allocation
 	p1_x = (int *)malloc((sizeof(int))*t);
 	p2_x = (int *)malloc((sizeof(int))*t);
 	p3_x = (int *)malloc((sizeof(int))*t);
@@ -24,9 +26,12 @@ int main(void)
 	p4_y = (int *)malloc((sizeof(int))*t);
 
 	for(i=0; i < t; i++){
+		// 3개의 꼭지점 정보 입력
 		scanf("%d %d",&p1_x[i], &p1_y[i]);
 		scanf("%d %d",&p2_x[i], &p2_y[i]);
 		scanf("%d %d",&p3_x[i], &p3_y[i]);
+		
+		// 남은 한 점의 좌표는  세점을 xor 연산한 결과
 		p4_x[i] = p1_x[i] ^ p2_x[i] ^ p3_x[i];
 		p4_y[i] = p1_y[i] ^ p2_y[i] ^ p3_y[i];
 	}
@@ -34,4 +39,14 @@ int main(void)
 	for(i=0; i<t; i++){
 		printf("%d %d\n", p4_x[i], p4_y[i]);
 	}
+
+	// 할당한 변수 해제
+	free(p1_x);
+	free(p2_x);
+	free(p3_x);
+	free(p1_y);
+	free(p2_y);
+	free(p3_y);
+	free(p4_x);
+	free(p4_y);
 }
